2adcf1fa9e7541ba61328e5c8fe48995828a05ed
Evan Peuvergne <evanpeuvergne@gmail.com>
2014-12-17 17:50:36 +0100

    Set up project structure

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+	
+	<!-- HEADER -->
+	<head>
+
+		<!-- METAS -->
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width">
+		<meta name="description" content="Fan de StarWars ? Venez découvrir vos personnages préférez sous un nouveau jour et mettez les au défi au travers de Battle pour les faire grimper dans la communauté." />
+
+		<!-- TITLE -->
+		<title>StarTruc</title>
+
+		<!-- CSS -->
+		<link rel="stylesheet" href="assets/css/reset.css">
+		<link rel="stylesheet" href="assets/css/main.css">
+
+	</head>
+	
+
+	<!-- BODY -->
+	<body>
+
+		hello
+		
+	</body>
+
+</html>
